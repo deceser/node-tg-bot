547aa7d07f8f576ee989a99b01c29bbf511f6f6c
deceser <aufhaummtobsc@gmail.com>
2025-03-28 12:25:02 +0200

    added libs: helmet, morgan, compression, rateLimit

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 // Configuration object with environment variables
 export const config = {
   BOT_TOKEN: process.env.BOT_TOKEN,
   PORT: process.env.PORT || 3000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
+  NODE_ENV: process.env.NODE_ENV || "development",
 };
